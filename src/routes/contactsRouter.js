3fd6d9eb431a39a264197cfce0136e1f929eb18f
Lina Parashchenko <blond2109@gmail.com>
2025-07-10 18:16:10 +0300

    fix mstks

--- src/routes/contactsRouter.js
+++ src/routes/contactsRouter.js
@@ -5,8 +5,21 @@ import { fetchContactById } from '../controllers/contactsController.js';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const contacts = await Contact.find();
-  res.json(contacts);
+  try {
+    const contacts = await Contact.find();
+
+    res.status(200).json({
+      status: 200,
+      message: 'Contacts successfully fetched!',
+      data: contacts,
+    });
+  } catch (error) {
+    console.error('GET /contacts error:', error.message);
+    res.status(500).json({
+      status: 500,
+      message: 'Server error',
+    });
+  }
 });
 
 router.get('/:contactId', fetchContactById);
--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ export function setupServer() {
 
   app.use(cors());
   app.use(express.json());
-  app.use('/api/contacts', contactsRouter);
+  app.use('/contacts', contactsRouter);
 
   app.use((req, res) => {
     res.status(404).json({ message: 'Not found' });
