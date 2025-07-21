1d089d49717803e4b0bef8d1d8f4dd41020b4780
Lina Parashchenko <blond2109@gmail.com>
2025-07-21 19:41:50 +0300

    fix bag

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -1,10 +1,14 @@
 import { isValidObjectId } from 'mongoose';
 import createError from 'http-errors';
-
 export const isValidId = (req, res, next) => {
-  const { contactId } = req.params;
-  if (!isValidObjectId(contactId)) {
+  const { id } = req.params;
+  console.log('Checking ID:', id);
+
+  if (!isValidObjectId(id)) {
+    console.log('Invalid ID');
     return next(createError(400, 'Invalid contact ID'));
   }
+
+  console.log('Valid ID');
   next();
 };
