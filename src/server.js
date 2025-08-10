3347908db1bc5d95792b292148f1a4946602170c
Lina Parashchenko <blond2109@gmail.com>
2025-08-10 12:07:41 +0300

    swagger docs

--- src/server.js
+++ src/server.js
@@ -8,26 +8,34 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import sessionRouter from './routes/sessionRouter.js';
 import authRouter from './routes/authRouter.js';
 import cookieParser from 'cookie-parser';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
+
 dotenv.config();
 
 const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-  },
+  transport: { target: 'pino-pretty' },
 });
+
+const swaggerDocument = YAML.load(
+  path.resolve(process.cwd(), 'docs', 'openapi.yaml'),
+);
 
 export function setupServer() {
   const app = express();
 
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.use(cookieParser());
   app.use(
     cors({
-      origin: 'http://localhost:3000',
+      origin: true,
       credentials: true,
     }),
   );
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+
   app.use('/contacts', contactsRouter);
   app.use('/auth', authRouter);
   app.use('/session', sessionRouter);
@@ -36,7 +44,6 @@ export function setupServer() {
   app.use(errorHandler);
 
   const PORT = process.env.PORT || 3000;
-
   app.listen(PORT, () => {
     logger.info(`Server is running on port ${PORT}`);
   });
